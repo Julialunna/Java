0e42f4c406d6e056a05f0013490d253e90c6fce7
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-09 23:26:23 -0300

    tirando getters  e setters desnecessários

--- Aulas/atividade-threads/atividade-threads/src/CorridaDeSapo.java
+++ Aulas/atividade-threads/atividade-threads/src/CorridaDeSapo.java
@@ -1,8 +1,8 @@
 public class CorridaDeSapo {
-    private int distanciaCorrida;
-    private int numSapos;
+    public int distanciaCorrida;
+    public int numSapos;
 
-    public int getDistanciaCorrida() {
+    /*public int getDistanciaCorrida() {
         return distanciaCorrida;
     }
     public void setDistanciaCorrida(int distanciaCorrida) {
@@ -13,23 +13,23 @@ public class CorridaDeSapo {
     }
     public void setNumSapos(int numSapos) {
         this.numSapos = numSapos;
-    }
+    }*/
 
     public CorridaDeSapo(int distanciaCorrida, int numSapos){
-        this.setNumSapos(numSapos);
-        this.setDistanciaCorrida(distanciaCorrida);
+        this.numSapos=numSapos;
+        this.distanciaCorrida=distanciaCorrida;
     }
 
     public void mostraInformacoes(){
-        System.out.println(this.getDistanciaCorrida());
-        System.out.println(this.getNumSapos());
+        System.out.println(this.distanciaCorrida);
+        System.out.println(this.numSapos);
     }
 
     public void criaSapos(){
         double puloMinimo=0.5;
 
-        for(double i=1;i<this.getNumSapos()+1;i++){
-            new Sapo(puloMinimo+i/4.0, i, this.getDistanciaCorrida()).start();
+        for(double i=1;i<this.numSapos+1;i++){
+            new Sapo(puloMinimo+i/4.0, i, this.distanciaCorrida).start();
         }
     
     }
