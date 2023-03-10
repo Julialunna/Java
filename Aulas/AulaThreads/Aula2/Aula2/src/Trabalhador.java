fdadf5d5dd9d9937e6c4b49eab45cb2e32ff8cfe
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-09 23:15:27 -0300

    fazendo atividade 2

---
+++ Aulas/AulaThreads/Aula2/Aula2/src/Trabalhador.java
@@ -0,0 +1,35 @@
+import java.io.Console;
+
+public class Trabalhador extends Thread{
+    String produto;
+    int tempo;
+    boolean parar=false;
+
+    public Trabalhador(String produto, int tempo){
+        this.produto=produto;
+        this.tempo= tempo;
+    }
+
+    public void parar(){
+        try 
+        { Thread.sleep(10000); } 
+        catch (InterruptedException ex) {
+            System.out.println("não rolou");
+        } 
+        this.parar=true;
+    }
+
+    public void run(){
+        for(int i=0;this.parar==false;i++){
+            System.out.println(i+" "+this.produto);
+            try{
+                sleep((long)(Math.random()*this.tempo));
+            }catch(InterruptedException e){}
+            /*if(i>90){
+                this.parar();
+            }
+            this.parar();*/
+        }
+        System.out.println("Terminei "+produto);
+    }
+}
